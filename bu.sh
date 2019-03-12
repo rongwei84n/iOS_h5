ea4bf53a4eec06176c65d683cb2ddd523e42b8ad
黄荣卫 <huangrongwei@cmhi.chinamobile.com>
2019-03-12 16:33:45 +0800

    增加国政通优化

--- bu.sh
+++ bu.sh
@@ -1,3 +1,3 @@
 tar -zcvf iOS_h5test.tar.gz hello-mui/
 
-scp iOS_h5test.tar.gz root@47.97.100.240:/root/deploy/lcs-backstage/front/dist
+scp iOS_h5test.tar.gz root@39.108.130.152:/root/deploy/cmcc-hebao/h5test/front/dist/
